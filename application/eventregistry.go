73cd65095f7971a26cd45ef6edd6515dfe85b6de
Christopher Huebner <huebner@fw-systeme.de>
2025-08-25 12:08:13 +0200

    Synchronized the event registry.

--- application/eventregistry.go
+++ application/eventregistry.go
@@ -2,30 +2,44 @@ package application
 
 import (
 	"fmt"
+	"sync"
 )
 
 type EventRegistry struct {
+	wg  *sync.WaitGroup
 	mem map[string]chan interface{}
 }
 
 func New() EventRegistry {
-	return EventRegistry{mem: make(map[string]chan interface{})}
+	return EventRegistry{
+		mem: make(map[string]chan interface{}),
+		wg:  &sync.WaitGroup{}}
 }
 
 func (bus EventRegistry) AddMew(typeId string) {
+	bus.wg.Wait()
+	bus.wg.Add(1)
+	defer bus.wg.Done()
 	bus.mem[typeId] = make(chan interface{})
 }
 
 func (bus EventRegistry) Fire(typeId string, event interface{}) error {
+	bus.wg.Wait()
+	bus.wg.Add(1)
+	defer bus.wg.Done()
 	ch, ok := bus.mem[typeId]
 	if !ok {
 		return fmt.Errorf("no subscriber for typeId: %s", typeId)
 	}
 	ch <- event
+	bus.wg.Done()
 	return nil
 }
 
 func (bus EventRegistry) Await(typeId string) (interface{}, error) {
+	bus.wg.Wait()
+	bus.wg.Add(1)
+	defer bus.wg.Done()
 	ch, ok := bus.mem[typeId]
 	if !ok {
 		return nil, fmt.Errorf("no channel for typeId: %s", typeId)
@@ -34,6 +48,9 @@ func (bus EventRegistry) Await(typeId string) (interface{}, error) {
 }
 
 func (bus EventRegistry) Listen(typeId string) (chan interface{}, error) {
+	bus.wg.Wait()
+	bus.wg.Add(1)
+	defer bus.wg.Done()
 	ch, ok := bus.mem[typeId]
 	if !ok {
 		return nil, fmt.Errorf("no channel for typeId: %s", typeId)
